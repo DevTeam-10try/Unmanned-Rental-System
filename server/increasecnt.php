0650b16a7746ad3898e7b5e59135d5b7f4145c45
HyunA-Kim <65345381+kha-github@users.noreply.github.com>
2020-10-10 01:29:50 +0900

    Update increasecnt.php

--- server/increasecnt.php
+++ server/increasecnt.php
@@ -1,14 +1,13 @@
 <?php
 		parse_str(getenv("QUERY_STRING"));
     		$conn = mysqli_connect("localhost","hahat17","10836gkwngus!", "db") or die("fail");
-    		//db 연결
+
     		$dbconn = mysqli_select_db($conn, $mysql_db);
-    		//charset UTF8
+
     		mysqli_query("set names utf 8");
-    		//쿼리문 작성
+
     		$query = "select alert from membership where card_id='".$card_id."'";
     		
-      		//쿼리보내고 결과를 변수에 저장
     		$result = mysqli_query($conn, $query);
 		$row = mysqli_fetch_array($result);
 		$input = $row[alert]+1 ;
